d5f08d148aa87dd76340149a2a13a971a25d404c
lcannafa <lcannafa@eafit.edu.co>
2019-05-30 16:15:57 -0500

    Inicio algo?

--- evaluador/src/Evaluador.cpp
+++ evaluador/src/Evaluador.cpp
@@ -14,4 +14,38 @@ using namespace std;
 
 int
 main(void) {
+
+    const char* n = "evaluator2";
+    // crea el espacio de memoria compartida, lanza error si ya existe
+    int fd = shm_open(n, O_RDWR | O_CREAT | O_EXCL, 0660);
+
+    if (fd < 0) {
+    cerr << "Error creando la memoria compartida: 1 "
+	 << errno << strerror(errno) << endl;
+    exit(1);
+  }
+
+
+  if (ftruncate(fd, sizeof(struct BufferI)) != 0) {
+    cerr << "Error creando la memoria compartida: 2 "
+	 << errno << strerror(errno) << endl;
+    exit(1);
+  }
+
+  void *dir;
+  // mapea el espacio de memoria reservado y guarda la dirreción en el apuntador dir
+  if ((dir = mmap(NULL, sizeof(struct elemento)*i*ie, PROT_READ | PROT_WRITE, MAP_SHARED,
+		  fd, 0)) == MAP_FAILED) {
+    cerr << "Error mapeando la memoria compartida: 3 "
+	 << errno << strerror(errno) << endl;
+    exit(1);
+  }
+
+  
+  struct BufferIE test;
+
+  struct BufferI *pBufferI = (struct BufferI *) dir;
+  pBufferI->bufferI[0] = test;
+
+  close(fd);
 }
