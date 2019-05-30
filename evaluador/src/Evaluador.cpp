bb01e4b8ae1f3c6cd5ab669c8cb081a6ecccf2c5
lcannafa <lcannafa@eafit.edu.co>
2019-05-30 10:49:27 -0500

    ProductorCOnsumidor ejemplos

--- evaluador/src/Evaluador.cpp
+++ evaluador/src/Evaluador.cpp
@@ -1,15 +1,17 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/mman.h>   
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <semaphore.h>
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include "elementos.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+int
+main(void) {
 }
