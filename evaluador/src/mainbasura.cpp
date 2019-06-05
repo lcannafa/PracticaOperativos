00f8b4250f8086764b19b437b87f74febfe60119
lcannafa <lcannafa@eafit.edu.co>
2019-06-05 08:52:27 -0500

    al mainbasura que testea, cambios pequeños

--- evaluador/src/mainbasura.cpp
+++ evaluador/src/mainbasura.cpp
@@ -3,13 +3,13 @@
 int main (){
     crearEspacio("buffer",3, 5, 10);
     struct registroentrada test;
-    test.bandeja = 1;
+    test.bandeja = 0;
     test.cantidad = 3;
     test.id = 8;
     test.tipo = 'D';
 
     struct registroentrada test2;
-    test2.bandeja = 1;
+    test2.bandeja = 2;
     test2.cantidad = 3;
     test2.id = 5;
     test2.tipo = 'D';
