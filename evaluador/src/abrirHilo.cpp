de857737525f2c795da4360b0c6b3dd5b1cfca7b
lcannafa <lcannafa@eafit.edu.co>
2019-06-06 16:02:44 -0500

    Creando hilos
    Co-authored-by: Pablo Bernal Moreno <pbernal2@eafit.edu.co>
    Co-authored-by: James Montoya <jamonto5@eafit.edu.co>

--- evaluador/src/abrirHilo.cpp
+++ evaluador/src/abrirHilo.cpp
@@ -1,4 +1,5 @@
 #include <cerrno>
+#include <stdio.h>
 #include <cstring>
 #include <sstream>
 #include <pthread.h>
@@ -7,33 +8,49 @@
 
 using namespace std;
 
-static void *procesador(void *arg);
-int main(int i)
-{
-    pthread_t hiloP[i];
-
-    string llen = "Hilo";
-    for (int n = 0; n < i; n++)
-    {
-        //pues no
-        ostringstream namellen;
-        namellen << llen << n;
-        string realNameLlen(namellen.str());
-        pthread_create(&hiloP[n], NULL, procesador, NULL);
-    }
-        
-}
-
-static void *procesador(void *arg)
+//Método debugging
+void *procesador(void *arg)
 {
 
+    printf("Corre 2\n");
     /* increment x to 100 */
     int *x_ptr = (int *)arg;
+    int x = 0;
     while(++(*x_ptr) < 100);
-
+    printf("x: %d\n",x);
+    ++x;
     printf("x increment finished\n");
 
     /* the function must return something - NULL will do */
     return NULL;
 
 }
+
+int main(int i)
+{
+    i=2;
+    int x = 0;
+    pthread_t hiloP[i];
+    printf("Corre x:%d\n", x);
+    
+    printf("Hace hilo x:%d\n",x);
+    string llen = "Hilo";
+    for (int n = 0; n < i; n++)
+    {
+        
+        ostringstream namellen;
+        namellen << llen << n;
+        string realNameLlen(namellen.str());
+        pthread_create(&hiloP[n], NULL, procesador, &x);
+    }
+    if (pthread_join(hiloP[0], NULL))
+    {
+
+        fprintf(stderr, "Error joining thread\n");
+        return 2;
+    }
+    printf("Hace hilo x:%d\n",x);
+        
+}
+
+
